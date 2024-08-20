731612e0aa53807d711c0b9cb208a1860dabe9b1
Khanloo <m.nadirkhanloo1380@gmail.com>
2024-08-20 12:39:37 +0330

    Feat : add daisyUi

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,20 @@ function App() {
   return (
     <>
       <div>
-        <h1 className="text-3xl font-bold underline">hello</h1>
+{/* Open the modal using document.getElementById('ID').showModal() method */}
+<button className="btn" onClick={()=>document.getElementById('my_modal_1').showModal()}>open modal</button>
+<dialog id="my_modal_1" className="modal">
+  <div className="modal-box">
+    <h3 className="font-bold text-lg">Hello!</h3>
+    <p className="py-4">Press ESC key or click the button below to close</p>
+    <div className="modal-action">
+      <form method="dialog">
+        {/* if there is a button in form, it will close the modal */}
+        <button className="btn">Close</button>
+      </form>
+    </div>
+  </div>
+</dialog>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+import daisyui from "./node_modules/daisyui"
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -7,6 +8,9 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui,],
+  daisyui: {
+    
+  },
 }
 
